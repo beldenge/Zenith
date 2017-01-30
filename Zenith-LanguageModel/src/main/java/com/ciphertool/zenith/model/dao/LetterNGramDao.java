f51c93589cec9d8e87fee6a9cb0509a00c5c7340
george <george@DESKTOP-GJQG94F.fios-router.home>
2017-01-30 14:18:19 -0500

    Parallelize findAll query

--- Zenith-LanguageModel/src/main/java/com/ciphertool/zenith/model/dao/LetterNGramDao.java
+++ Zenith-LanguageModel/src/main/java/com/ciphertool/zenith/model/dao/LetterNGramDao.java
@@ -19,24 +19,70 @@
 
 package com.ciphertool.zenith.model.dao;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.query.BasicQuery;
 
 import com.ciphertool.zenith.model.markov.NGramIndexNode;
+import com.mongodb.Cursor;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.ParallelScanOptions;
 
 public class LetterNGramDao {
 	private static final String	COLLECTION_WITH_SPACES		= "letterNGrams_withSpaces";
 	private static final String	COLLECTION_WITHOUT_SPACES	= "letterNGrams_withoutSpaces";
+	private static final String	ID_KEY						= "id";
+	private static final String	LEVEL_KEY					= "level";
+	private static final String	COUNT_KEY					= "count";
+	private static final String	PROBABILITY_KEY				= "probability";
+	private static final String	CONDITIONAL_PROBABILITY_KEY	= "conditionalProbability";
+	private static final String	CUMULATIVE_STRING_KEY		= "cumulativeString";
 
 	private MongoOperations		mongoOperations;
+	private int					batchSize;
+	private int					numCursors;
 
 	public List<NGramIndexNode> findAll(int minimumCount, boolean includeWordBoundaries) {
-		BasicQuery query = new BasicQuery("{ count : { $gte : " + minimumCount + " } }");
+		DBCollection collection = mongoOperations.getCollection((includeWordBoundaries ? COLLECTION_WITH_SPACES : COLLECTION_WITHOUT_SPACES));
+		List<Cursor> cursors = collection.parallelScan(ParallelScanOptions.builder().batchSize(batchSize).numCursors(numCursors).build());
 
-		return mongoOperations.find(query, NGramIndexNode.class, (includeWordBoundaries ? COLLECTION_WITH_SPACES : COLLECTION_WITHOUT_SPACES));
+		List<NGramIndexNode> nodesToReturn = new ArrayList<>();
+
+		DBObject next;
+		NGramIndexNode nextNode;
+
+		for (Cursor cursor : cursors) {
+			while (cursor.hasNext()) {
+				next = cursor.next();
+
+				if (((long) next.get(COUNT_KEY)) >= minimumCount) {
+					nextNode = new NGramIndexNode(null, (String) next.get(CUMULATIVE_STRING_KEY),
+							(int) next.get(LEVEL_KEY));
+
+					nextNode.setId((ObjectId) next.get(ID_KEY));
+					nextNode.setCount((long) next.get(COUNT_KEY));
+
+					if (next.containsField(PROBABILITY_KEY)) {
+						nextNode.setProbability(new BigDecimal((String) next.get(PROBABILITY_KEY)));
+					}
+
+					if (next.containsField(CONDITIONAL_PROBABILITY_KEY)) {
+						nextNode.setConditionalProbability(new BigDecimal(
+								(String) next.get(CONDITIONAL_PROBABILITY_KEY)));
+					}
+
+					nodesToReturn.add(nextNode);
+				}
+			}
+		}
+
+		return nodesToReturn;
 	}
 
 	public long countLessThan(int minimumCount, boolean includeWordBoundaries) {
@@ -58,5 +104,23 @@ public class LetterNGramDao {
 	@Required
 	public void setMongoTemplate(MongoOperations mongoOperations) {
 		this.mongoOperations = mongoOperations;
+	}
+
+	/**
+	 * @param batchSize
+	 *            the batchSize to set
+	 */
+	@Required
+	public void setBatchSize(int batchSize) {
+		this.batchSize = batchSize;
+	}
+
+	/**
+	 * @param numCursors
+	 *            the numCursors to set
+	 */
+	@Required
+	public void setNumCursors(int numCursors) {
+		this.numCursors = numCursors;
 	}
 }
