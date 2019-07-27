4bbd2900f3ded8c2e0a18a753b8e511d16bcadd3
George Belden <beldenge@gmail.com>
2019-07-27 15:42:47 -0400

    Add period transformers

---
+++ zenith-inference/src/test/java/com/ciphertool/zenith/inference/transformer/UnwrapPeriodCipherTransformerTest.java
@@ -0,0 +1,129 @@
+/**
+ * Copyright 2017-2019 George Belden
+ *
+ * This file is part of Zenith.
+ *
+ * Zenith is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Zenith is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Zenith. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.ciphertool.zenith.inference.transformer;
+
+import com.ciphertool.zenith.inference.entities.Cipher;
+import com.ciphertool.zenith.inference.entities.Ciphertext;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class UnwrapPeriodCipherTransformerTest {
+    @Test
+    public void testTransform() {
+        UnwrapPeriodCipherTransformer cipherTransformer = new UnwrapPeriodCipherTransformer(5);
+
+        Cipher cipher = new Cipher("tomato", 7, 6);
+
+        cipher.addCiphertextCharacter(new Ciphertext(0, "T"));
+        cipher.addCiphertextCharacter(new Ciphertext(1, "M"));
+        cipher.addCiphertextCharacter(new Ciphertext(2, "S"));
+        cipher.addCiphertextCharacter(new Ciphertext(3, "N"));
+        cipher.addCiphertextCharacter(new Ciphertext(4, "H"));
+        cipher.addCiphertextCharacter(new Ciphertext(5, "H"));
+        cipher.addCiphertextCharacter(new Ciphertext(6, "D"));
+        cipher.addCiphertextCharacter(new Ciphertext(7, "I"));
+        cipher.addCiphertextCharacter(new Ciphertext(8, "X"));
+        cipher.addCiphertextCharacter(new Ciphertext(9, "H"));
+        cipher.addCiphertextCharacter(new Ciphertext(10, "A"));
+        cipher.addCiphertextCharacter(new Ciphertext(11, "A"));
+        cipher.addCiphertextCharacter(new Ciphertext(12, "T"));
+        cipher.addCiphertextCharacter(new Ciphertext(13, "E"));
+        cipher.addCiphertextCharacter(new Ciphertext(14, "T"));
+        cipher.addCiphertextCharacter(new Ciphertext(15, "E"));
+        cipher.addCiphertextCharacter(new Ciphertext(16, "L"));
+        cipher.addCiphertextCharacter(new Ciphertext(17, "X"));
+        cipher.addCiphertextCharacter(new Ciphertext(18, "E"));
+        cipher.addCiphertextCharacter(new Ciphertext(19, "T"));
+        cipher.addCiphertextCharacter(new Ciphertext(20, "P"));
+        cipher.addCiphertextCharacter(new Ciphertext(21, "I"));
+        cipher.addCiphertextCharacter(new Ciphertext(22, "N"));
+        cipher.addCiphertextCharacter(new Ciphertext(23, "S"));
+        cipher.addCiphertextCharacter(new Ciphertext(24, "F"));
+        cipher.addCiphertextCharacter(new Ciphertext(25, "Y"));
+        cipher.addCiphertextCharacter(new Ciphertext(26, "T"));
+        cipher.addCiphertextCharacter(new Ciphertext(27, "O"));
+        cipher.addCiphertextCharacter(new Ciphertext(28, "L"));
+        cipher.addCiphertextCharacter(new Ciphertext(29, "N"));
+        cipher.addCiphertextCharacter(new Ciphertext(30, "I"));
+        cipher.addCiphertextCharacter(new Ciphertext(31, "H"));
+        cipher.addCiphertextCharacter(new Ciphertext(32, "A"));
+        cipher.addCiphertextCharacter(new Ciphertext(33, "X"));
+        cipher.addCiphertextCharacter(new Ciphertext(34, "O"));
+        cipher.addCiphertextCharacter(new Ciphertext(35, "I"));
+        cipher.addCiphertextCharacter(new Ciphertext(36, "A"));
+        cipher.addCiphertextCharacter(new Ciphertext(37, "T"));
+        cipher.addCiphertextCharacter(new Ciphertext(38, "G"));
+        cipher.addCiphertextCharacter(new Ciphertext(39, "A"));
+        cipher.addCiphertextCharacter(new Ciphertext(40, "M"));
+        cipher.addCiphertextCharacter(new Ciphertext(41, "X"));
+
+        Cipher transformed = cipherTransformer.transform(cipher);
+
+        assertEquals(42, cipher.length());
+        assertEquals(6, transformed.getColumns());
+        assertEquals(7, transformed.getRows());
+
+        assertEquals("T", transformed.getCiphertextCharacters().get(0).getValue());
+        assertEquals("H", transformed.getCiphertextCharacters().get(1).getValue());
+        assertEquals("E", transformed.getCiphertextCharacters().get(2).getValue());
+        assertEquals("T", transformed.getCiphertextCharacters().get(3).getValue());
+        assertEquals("O", transformed.getCiphertextCharacters().get(4).getValue());
+        assertEquals("M", transformed.getCiphertextCharacters().get(5).getValue());
+        assertEquals("A", transformed.getCiphertextCharacters().get(6).getValue());
+        assertEquals("T", transformed.getCiphertextCharacters().get(7).getValue());
+        assertEquals("O", transformed.getCiphertextCharacters().get(8).getValue());
+        assertEquals("I", transformed.getCiphertextCharacters().get(9).getValue());
+        assertEquals("S", transformed.getCiphertextCharacters().get(10).getValue());
+        assertEquals("A", transformed.getCiphertextCharacters().get(11).getValue());
+        assertEquals("P", transformed.getCiphertextCharacters().get(12).getValue());
+        assertEquals("L", transformed.getCiphertextCharacters().get(13).getValue());
+        assertEquals("A", transformed.getCiphertextCharacters().get(14).getValue());
+        assertEquals("N", transformed.getCiphertextCharacters().get(15).getValue());
+        assertEquals("T", transformed.getCiphertextCharacters().get(16).getValue());
+        assertEquals("I", transformed.getCiphertextCharacters().get(17).getValue());
+        assertEquals("N", transformed.getCiphertextCharacters().get(18).getValue());
+        assertEquals("T", transformed.getCiphertextCharacters().get(19).getValue());
+        assertEquals("H", transformed.getCiphertextCharacters().get(20).getValue());
+        assertEquals("E", transformed.getCiphertextCharacters().get(21).getValue());
+        assertEquals("N", transformed.getCiphertextCharacters().get(22).getValue());
+        assertEquals("I", transformed.getCiphertextCharacters().get(23).getValue());
+        assertEquals("G", transformed.getCiphertextCharacters().get(24).getValue());
+        assertEquals("H", transformed.getCiphertextCharacters().get(25).getValue());
+        assertEquals("T", transformed.getCiphertextCharacters().get(26).getValue());
+        assertEquals("S", transformed.getCiphertextCharacters().get(27).getValue());
+        assertEquals("H", transformed.getCiphertextCharacters().get(28).getValue());
+        assertEquals("A", transformed.getCiphertextCharacters().get(29).getValue());
+        assertEquals("D", transformed.getCiphertextCharacters().get(30).getValue());
+        assertEquals("E", transformed.getCiphertextCharacters().get(31).getValue());
+        assertEquals("F", transformed.getCiphertextCharacters().get(32).getValue());
+        assertEquals("A", transformed.getCiphertextCharacters().get(33).getValue());
+        assertEquals("M", transformed.getCiphertextCharacters().get(34).getValue());
+        assertEquals("I", transformed.getCiphertextCharacters().get(35).getValue());
+        assertEquals("L", transformed.getCiphertextCharacters().get(36).getValue());
+        assertEquals("Y", transformed.getCiphertextCharacters().get(37).getValue());
+        assertEquals("X", transformed.getCiphertextCharacters().get(38).getValue());
+        assertEquals("X", transformed.getCiphertextCharacters().get(39).getValue());
+        assertEquals("X", transformed.getCiphertextCharacters().get(40).getValue());
+        assertEquals("X", transformed.getCiphertextCharacters().get(41).getValue());
+
+        System.out.println(transformed);
+    }
+}
