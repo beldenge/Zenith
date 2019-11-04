6ac2ed5e0ca65bf19d69f3438898b676b6422c97
George Belden <beldenge@gmail.com>
2019-11-03 21:25:29 -0500

    Add links to future pages and fix small UI bugs

--- zenith-ui/src/main/resources/static/js/sb-admin-2.js
+++ zenith-ui/src/main/resources/static/js/sb-admin-2.js
@@ -2,33 +2,33 @@
   "use strict"; // Start of use strict
 
   // Toggle the side navigation
-  $("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {
+  $(document).on('click', '#sidebarToggle, #sidebarToggleTop', function (e) {
     $("body").toggleClass("sidebar-toggled");
     $(".sidebar").toggleClass("toggled");
     if ($(".sidebar").hasClass("toggled")) {
       $('.sidebar .collapse').collapse('hide');
-    };
+    }
   });
 
   // Close any open menu accordions when window is resized below 768px
-  $(window).resize(function() {
+  $(window).resize(function () {
     if ($(window).width() < 768) {
       $('.sidebar .collapse').collapse('hide');
-    };
+    }
   });
 
   // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
-  $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function(e) {
+  $(document).on('mousewheel DOMMouseScroll wheel', 'body.fixed-nav .sidebar', function (e) {
     if ($(window).width() > 768) {
       var e0 = e.originalEvent,
-        delta = e0.wheelDelta || -e0.detail;
+          delta = e0.wheelDelta || -e0.detail;
       this.scrollTop += (delta < 0 ? 1 : -1) * 30;
       e.preventDefault();
     }
   });
 
   // Scroll to top button appear
-  $(document).on('scroll', function() {
+  $(document).on('scroll', function () {
     var scrollDistance = $(this).scrollTop();
     if (scrollDistance > 100) {
       $('.scroll-to-top').fadeIn();
@@ -38,7 +38,7 @@
   });
 
   // Smooth scrolling using jQuery easing
-  $(document).on('click', 'a.scroll-to-top', function(e) {
+  $(document).on('click', 'a.scroll-to-top', function (e) {
     var $anchor = $(this);
     $('html, body').stop().animate({
       scrollTop: ($($anchor.attr('href')).offset().top)
