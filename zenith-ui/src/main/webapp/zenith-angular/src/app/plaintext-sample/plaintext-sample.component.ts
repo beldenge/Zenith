e620dc84c744d22fd1a88f34cab35d8881130885
George Belden <beldenge@gmail.com>
2020-02-27 22:44:06 -0500

    Make sample plaintext persist across component inits

--- zenith-ui/src/main/webapp/zenith-angular/src/app/plaintext-sample/plaintext-sample.component.ts
+++ zenith-ui/src/main/webapp/zenith-angular/src/app/plaintext-sample/plaintext-sample.component.ts
@@ -1,18 +1,28 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterContentInit, Component, Input, OnInit } from '@angular/core';
 import { ConfigurationService } from "../configuration.service";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: 'app-plaintext-sample',
   templateUrl: './plaintext-sample.component.html',
   styleUrls: ['./plaintext-sample.component.css']
 })
-export class PlaintextSampleComponent implements OnInit {
-  originalSample = ConfigurationService.DEFAULT_SAMPLE_PLAINTEXT;
+export class PlaintextSampleComponent implements OnInit, AfterContentInit  {
   @Input() transformedSample: string;
+  sample: string;
+  samplePlaintextSubscription: Subscription;
 
   constructor(private configurationService: ConfigurationService) { }
 
   ngOnInit() {
+    this.samplePlaintextSubscription = this.configurationService.getSamplePlaintextAsObservable().subscribe(sample => {
+      this.sample = sample;
+    });
+  }
+
+  ngAfterContentInit() {
+    // We just want the initial value, then don't take updates anymore
+    this.samplePlaintextSubscription.unsubscribe();
   }
 
   reset(sampleEditor: HTMLElement) {
