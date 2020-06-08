9a04e16082a35731ecb9d6679b52c5347a45c165
George Belden <beldenge@gmail.com>
2020-06-07 23:35:55 -0400

    Increment version to 2.1.2-SNAPSHOT

--- zenith-ui/src/environments/environment.prod.ts
+++ zenith-ui/src/environments/environment.prod.ts
@@ -20,6 +20,6 @@
 export const environment = {
   production: true,
   apiUrlBase: 'api', // relative URL
-  applicationVersion: '2.1.1',
+  applicationVersion: '2.1.2-SNAPSHOT',
   googleAnalyticsTrackingId: 'UA-159370258-1'
 };
