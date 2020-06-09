36212225bc6f9f6626dfe886f705eeb0a9140699
George Belden <beldenge@gmail.com>
2020-06-08 23:45:19 -0400

    Release version 2.1.2

--- zenith-ui/src/environments/environment.prod.ts
+++ zenith-ui/src/environments/environment.prod.ts
@@ -20,6 +20,6 @@
 export const environment = {
   production: true,
   apiUrlBase: 'api', // relative URL
-  applicationVersion: '2.1.2-SNAPSHOT',
+  applicationVersion: '2.1.2',
   googleAnalyticsTrackingId: 'UA-159370258-1'
 };
--- zenith-ui/src/environments/environment.ts
+++ zenith-ui/src/environments/environment.ts
@@ -24,7 +24,7 @@
 export const environment = {
   production: false,
   apiUrlBase: 'api', // relative URL
-  applicationVersion: '2.1.2-SNAPSHOT',
+  applicationVersion: '2.1.2',
   googleAnalyticsTrackingId: 'UA-159370258-1'
 };
 
