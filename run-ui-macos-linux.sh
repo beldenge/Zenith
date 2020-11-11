74495d8be2a5d5e1d3076540e555451a4e376c41
George Belden <beldenge@gmail.com>
2020-11-11 00:15:11 -0500

    Make run scripts work from project root and update Spring Boot version

--- run-ui-macos-linux.sh
+++ run-ui-macos-linux.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
 JAVA_OPTS="$JAVA_OTPS -XX:+UseParallelGC -XX:ParallelGCThreads=2 -Xms2G -Xmx2G -XX:MaxMetaspaceSize=512M"
+JAR_FILENAME="zenith-ui-2.1.4-SNAPSHOT.jar"
+JAR_PATH=
 
-java $JAVA_OPTS -jar zenith-ui-2.1.4-SNAPSHOT.jar
+if [ ! -e $JAR_FILENAME ]; then
+  JAR_PATH="zenith-ui/target/"
+fi
+
+java $JAVA_OPTS -jar $JAR_PATH$JAR_FILENAME
--- run-ui-windows.bat
+++ run-ui-windows.bat
@@ -1,3 +1,9 @@
 set JAVA_OPTS=%JAVA_OPTS% -XX:+UseParallelGC -XX:ParallelGCThreads=2 -Xms2G -Xmx2G -XX:MaxMetaspaceSize=512M
+set JAR_FILENAME=zenith-ui-2.1.4-SNAPSHOT.jar
+set JAR_PATH=
 
-java %JAVA_OPTS% -jar zenith-ui-2.1.4-SNAPSHOT.jar
+if not exist %JAR_FILENAME% (
+    set JAR_PATH=zenith-ui\target\
+)
+
+java %JAVA_OPTS% -jar %JAR_PATH%%JAR_FILENAME%
