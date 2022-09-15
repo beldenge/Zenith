da66e8c300f5edc3ba51c11adb34811f77303233
George Belden <beldenge@gmail.com>
2022-09-14 23:30:44 -0400

    Remove unused test classes

--- zenith-ui/src/app/spacify.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SpacifyPipe } from './spacify.pipe';
-
-describe('SpacifyPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SpacifyPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
