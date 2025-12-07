6c08cfd149ce61b18c2fc1b593e9ff2e852a1993
George Belden <beldenge@gmail.com>
2025-12-07 08:49:09 -0500

    Upgrade to Angular 21

--- zenith-ui/src/main.ts
+++ zenith-ui/src/main.ts
@@ -17,7 +17,7 @@
  * Zenith. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -27,5 +27,5 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
   .catch(err => console.error(err));
